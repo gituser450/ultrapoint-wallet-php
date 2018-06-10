f1b561903c768921253292ad9943187f612a09fb
one4a11 <one4a11@msgsafe.io>
2018-06-10 15:53:24 +0100

    update example with new changes

--- example.php
+++ example.php
@@ -4,9 +4,15 @@ require "vendor/autoload.php";
 use Ultrapoint\Wallet;
 
 $wallet = new Ultrapoint\Wallet();
+# or with rpc authentification needed
+// $wallet = new Ultrapoint\Wallet('127.0.0.1', 17072, 'upxrpcuser', 'rpcpasswd');
+
+# used when rpc wallet is started with `--wallet-dir` option
+// echo $wallet->create_wallet('upx_testy', 'testy');
+// echo $wallet->open_wallet('upx_testy', 'testy');
 
 $destination1 = (object) [
-    'amount' => '0.1',
+    'amount' => '1',
     'address' => '7Ey8jHDkWqYDSpoSssv5EmAcsXCct4hum4mhHxT6ruaof9C7JM1ekjsYFa8dQEUL4QMai15akL2az2Me48ShgNMWV3yBkSV'
 ];
 
@@ -16,3 +22,4 @@ $options = [
 
 echo $wallet->transfer($options);
 
+// echo $wallet->getAddress($options);
